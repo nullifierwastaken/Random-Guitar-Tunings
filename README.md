969b8ab20016f16e34b3fd4ef784e84ed0d9bdf0
nullifierwastaken <thomas.z.li.hangouts@gmail.com>
2026-01-18 04:10:20 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Guitar Tunings
+## What this does
+This is a shitty project I made because I was bored at 3-ish AM, it generates random guitar tunings. That's it
---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+low_e = random.randint(0,11)
+a = random.randint(0,11)
+d = random.randint(0,11)
+g = random.randint(0,11)
+b = random.randint(0,11)
+high_e = random.randint(0,11)
+
+print(f'{notes[low_e]} |------------------------------------\n
+        {notes[a]} |------------------------------------\n
+        {notes[d]} |------------------------------------\n
+        {notes[g]} |------------------------------------\n
+        {notes[b]} |------------------------------------\n
+        {notes[high_e]} |------------------------------------')
+
